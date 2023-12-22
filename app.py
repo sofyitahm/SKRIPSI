e5b797bd4d28709eaf53370b879d4f90944a0928
sofyitahm <130208937+sofyitahm@users.noreply.github.com>
2023-12-22 15:55:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,25 @@ import pandas as pd
 st.write("""
 # My first app
 Hello *world!*
+I will make a PieChart wuhu
 """)
  
+import streamlit as st
+import pandas as pd
+
+# Membaca CSV
 df = pd.read_csv("my_data.csv")
-st.line_chart(df)
+
+# Menampilkan data frame
+st.write("Data Frame:", df)
+
+# Memilih kolom-kolom yang ingin digunakan untuk pie chart
+selected_columns = st.multiselect("Pilih kolom untuk Pie Chart", df.columns)
+
+# Memeriksa apakah ada kolom yang dipilih
+if selected_columns:
+    # Menampilkan diagram pie chart
+    st.pie_chart(df[selected_columns])
+else:
+    st.warning("Pilih setidaknya satu kolom untuk membuat pie chart.")
+
