653b529a5ec28c346dcaedac2fe285fd421c8397
sofyitahm <130208937+sofyitahm@users.noreply.github.com>
2023-12-22 16:10:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
 
 # Membaca CSV
 df = pd.read_csv("my_data.csv")
@@ -9,9 +8,6 @@ df = pd.read_csv("my_data.csv")
 # Menampilkan data frame
 st.write("Data Frame:", df)
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, labels=kategori, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-st.pyplot(fig1)
+# Membuat diagram pie chart dengan plotly
+fig = px.pie(df, names='kategori', values='jumlah', title='Pie Chart Aspek Performa')
+st.plotly_chart(fig)
